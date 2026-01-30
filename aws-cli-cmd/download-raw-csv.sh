5931829a51da1ee97a8b1e59595c8a5a3fc73eb8
allenchen <allen.chen@iress.com>
2026-01-31 09:37:57 +1100

    add script to download raw CSV data from S3

---
+++ aws-cli-cmd/download-raw-csv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+BUCKET_NAME="instacart-data-eng-project"
+
+curl -o raw_data/aisles.csv https://instacart-raw-data.s3.ap-southeast-2.amazonaws.com/raw/aisles.csv
+curl -o raw_data/departments.csv https://instacart-raw-data.s3.ap-southeast-2.amazonaws.com/raw/departments.csv
+curl -o raw_data/products.csv https://instacart-raw-data.s3.ap-southeast-2.amazonaws.com/raw/products.csv
+curl -o raw_data/orders.csv https://instacart-raw-data.s3.ap-southeast-2.amazonaws.com/raw/orders.csv
+curl -o raw_data/order_products__prior.csv https://instacart-raw-data.s3.ap-southeast-2.amazonaws.com/raw/order_products__prior.csv
